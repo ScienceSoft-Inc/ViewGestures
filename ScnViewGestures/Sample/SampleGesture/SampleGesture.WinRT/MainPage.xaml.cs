e721f617b00b057c9441564ef73d17d4bc5c1ea0
dhlukhatarenka <dhlukhatarenka@scnsoft.com>
2016-10-24 17:51:13 +0300

    WinPhone 8.1 Silverlight changed to WinPhone 8.1 RT

---
+++ ScnViewGestures/Sample/SampleGesture/SampleGesture.WinRT/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ScnViewGestures.Plugin.Forms.WinRT.Renderers;
+
+namespace SampleGesture.WinRT
+{
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			ViewGesturesRenderer.Init();
+			LoadApplication(new SampleGesture.App());
+		}
+	}
+}
