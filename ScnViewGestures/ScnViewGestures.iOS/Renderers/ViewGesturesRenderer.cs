aeea426c0dd3d47808ed9ae86011ddab3d2e438c
Alexander Pivovar <apivovar@scnsoft.com>
2018-06-08 18:26:46 +0300

    fix NullReferenceException in iOS

--- ScnViewGestures/ScnViewGestures.iOS/Renderers/ViewGesturesRenderer.cs
+++ ScnViewGestures/ScnViewGestures.iOS/Renderers/ViewGesturesRenderer.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Foundation;
+using ScnViewGestures.Plugin.Forms;
+using ScnViewGestures.Plugin.Forms.iOS.Renderers;
+using System;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
-using ScnViewGestures.Plugin.Forms;
-using ScnViewGestures.Plugin.Forms.iOS.Renderers;
-using CoreGraphics;
 
 [assembly: ExportRenderer(typeof(ViewGestures), typeof(ViewGesturesRenderer))]
 
@@ -18,27 +13,28 @@ namespace ScnViewGestures.Plugin.Forms.iOS.Renderers
     public class ViewGesturesRenderer : ViewRenderer
     {
         // Used for registration with dependency service
-        public async static void Init()
+        public static async void Init()
         {
             var temp = DateTime.Now;
         }
 
-        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
         {
             base.OnElementChanged(e);
-            var viewGesture = (ViewGestures)Element;
 
-            var tapGestureRecognizer = new TapGestureRecognizer()
+            var viewGesture = (ViewGestures) e.NewElement;
+
+            var tapGestureRecognizer = new TapGestureRecognizer
             {
-                OnTouchesBegan = ((x, y) => viewGesture.OnTouchBegan(x, y)),
-                OnTouchesEnded = (() => viewGesture.OnTouchEnded()),
-                OnTap = ((x, y) => viewGesture.OnTap(x, y)),
+                OnTouchesBegan = (x, y) => viewGesture.OnTouchBegan(x, y),
+                OnTouchesEnded = () => viewGesture.OnTouchEnded(),
+                OnTap = (x, y) => viewGesture.OnTap(x, y)
             };
 
             var longPressGestureRecognizer = new LongPressGestureRecognizer(() => viewGesture.OnLongTap())
             {
-                OnTouchesBegan = ((x, y) => viewGesture.OnTouchBegan(x, y)),
-                OnTouchesEnded = (() => viewGesture.OnTouchEnded()),
+                OnTouchesBegan = (x, y) => viewGesture.OnTouchBegan(x, y),
+                OnTouchesEnded = () => viewGesture.OnTouchEnded()
             };
             
             #region SwipeGestureRecognizer
@@ -46,89 +42,93 @@ namespace ScnViewGestures.Plugin.Forms.iOS.Renderers
             {
                 Direction = UISwipeGestureRecognizerDirection.Left,
 
-                OnTouchesBegan = ((x, y) => viewGesture.OnTouchBegan(x, y)),
-                OnTouchesEnded = (() => viewGesture.OnTouchEnded()),
+                OnTouchesBegan = (x, y) => viewGesture.OnTouchBegan(x, y),
+                OnTouchesEnded = () => viewGesture.OnTouchEnded()
             };
 
             var swipeRightGestureRecognizer = new SwipeGestureRecognizer(() => viewGesture.OnSwipeRight())
             {
                 Direction = UISwipeGestureRecognizerDirection.Right,
 
-                OnTouchesBegan = ((x, y) => viewGesture.OnTouchBegan(x, y)),
-                OnTouchesEnded = (() => viewGesture.OnTouchEnded()),
+                OnTouchesBegan = (x, y) => viewGesture.OnTouchBegan(x, y),
+                OnTouchesEnded = () => viewGesture.OnTouchEnded()
             };
 
             var swipeUpGestureRecognizer = new SwipeGestureRecognizer(() => viewGesture.OnSwipeUp())
             {
                 Direction = UISwipeGestureRecognizerDirection.Up,
 
-                OnTouchesBegan = ((x, y) => viewGesture.OnTouchBegan(x, y)),
-                OnTouchesEnded = (() => viewGesture.OnTouchEnded()),
+                OnTouchesBegan = (x, y) => viewGesture.OnTouchBegan(x, y),
+                OnTouchesEnded = () => viewGesture.OnTouchEnded()
             };
 
             var swipeDownGestureRecognizer = new SwipeGestureRecognizer(() => viewGesture.OnSwipeDown())
             {
                 Direction = UISwipeGestureRecognizerDirection.Down,
 
-                OnTouchesBegan = ((x, y) => viewGesture.OnTouchBegan(x, y)),
-                OnTouchesEnded = (() => viewGesture.OnTouchEnded()),
+                OnTouchesBegan = (x, y) => viewGesture.OnTouchBegan(x, y),
+                OnTouchesEnded = () => viewGesture.OnTouchEnded()
             };
             #endregion
 
             #region DragGestureRecognizer
-            var dragGestureRecognizer = new DragGestureRecognizer()
+            var dragGestureRecognizer = new DragGestureRecognizer
             {
 
-                OnTouchesBegan = ((x, y) => viewGesture.OnTouchBegan(x, y)),
-                OnTouchesEnded = (() => viewGesture.OnTouchEnded()),
-                OnDrag = ((x, y) => viewGesture.OnDrag(x, y)),
+                OnTouchesBegan = (x, y) => viewGesture.OnTouchBegan(x, y),
+                OnTouchesEnded = () => viewGesture.OnTouchEnded(),
+                OnDrag = (x, y) => viewGesture.OnDrag(x, y)
             };
 
-            //from iOS Developer Library (Gesture Recognizers)
-            //...For your view to recognize both swipes and pans, you want the swipe gesture recognizer to analyze the touch event before the pan gesture recognizer does...
-            if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeLeft) != 0)
-                dragGestureRecognizer.RequireGestureRecognizerToFail(swipeLeftGestureRecognizer);
-            if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeRight) != 0)
-                dragGestureRecognizer.RequireGestureRecognizerToFail(swipeRightGestureRecognizer);
-            if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeUp) != 0)
-                dragGestureRecognizer.RequireGestureRecognizerToFail(swipeUpGestureRecognizer);
-            if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeDown) != 0)
-                dragGestureRecognizer.RequireGestureRecognizerToFail(swipeDownGestureRecognizer);
+            if (viewGesture != null)
+            {
+                //from iOS Developer Library (Gesture Recognizers)
+                //...For your view to recognize both swipes and pans, you want the swipe gesture recognizer to analyze the touch event before the pan gesture recognizer does...
+                if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeLeft) != 0)
+                    dragGestureRecognizer.RequireGestureRecognizerToFail(swipeLeftGestureRecognizer);
+                if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeRight) != 0)
+                    dragGestureRecognizer.RequireGestureRecognizerToFail(swipeRightGestureRecognizer);
+                if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeUp) != 0)
+                    dragGestureRecognizer.RequireGestureRecognizerToFail(swipeUpGestureRecognizer);
+                if ((viewGesture.SupportGestures & ViewGestures.GestureType.gtSwipeDown) != 0)
+                    dragGestureRecognizer.RequireGestureRecognizerToFail(swipeDownGestureRecognizer);
+            }
+
             #endregion
 
             if (e.NewElement == null)
             {
                 if (tapGestureRecognizer != null)
-                    this.RemoveGestureRecognizer(tapGestureRecognizer);
+                    RemoveGestureRecognizer(tapGestureRecognizer);
 
                 if (longPressGestureRecognizer != null)
-                    this.RemoveGestureRecognizer(longPressGestureRecognizer);
+                    RemoveGestureRecognizer(longPressGestureRecognizer);
 
                 if (swipeLeftGestureRecognizer != null)
-                    this.RemoveGestureRecognizer(swipeLeftGestureRecognizer);
+                    RemoveGestureRecognizer(swipeLeftGestureRecognizer);
 
                 if (swipeRightGestureRecognizer != null)
-                    this.RemoveGestureRecognizer(swipeRightGestureRecognizer);
+                    RemoveGestureRecognizer(swipeRightGestureRecognizer);
                 
                 if (swipeUpGestureRecognizer != null)
-                    this.RemoveGestureRecognizer(swipeUpGestureRecognizer);
+                    RemoveGestureRecognizer(swipeUpGestureRecognizer);
                 
                 if (swipeDownGestureRecognizer != null)
-                    this.RemoveGestureRecognizer(swipeDownGestureRecognizer);
+                    RemoveGestureRecognizer(swipeDownGestureRecognizer);
 
                 if (dragGestureRecognizer != null)
-                    this.RemoveGestureRecognizer(dragGestureRecognizer);
+                    RemoveGestureRecognizer(dragGestureRecognizer);
             }
 
             if (e.OldElement == null)
             {
-                this.AddGestureRecognizer(tapGestureRecognizer);
-                this.AddGestureRecognizer(longPressGestureRecognizer);
-                this.AddGestureRecognizer(swipeLeftGestureRecognizer);
-                this.AddGestureRecognizer(swipeRightGestureRecognizer);
-                this.AddGestureRecognizer(swipeUpGestureRecognizer);
-                this.AddGestureRecognizer(swipeDownGestureRecognizer);
-                this.AddGestureRecognizer(dragGestureRecognizer);
+                AddGestureRecognizer(tapGestureRecognizer);
+                AddGestureRecognizer(longPressGestureRecognizer);
+                AddGestureRecognizer(swipeLeftGestureRecognizer);
+                AddGestureRecognizer(swipeRightGestureRecognizer);
+                AddGestureRecognizer(swipeUpGestureRecognizer);
+                AddGestureRecognizer(swipeDownGestureRecognizer);
+                AddGestureRecognizer(dragGestureRecognizer);
             }
         }
 
@@ -137,15 +137,15 @@ namespace ScnViewGestures.Plugin.Forms.iOS.Renderers
             public override void TouchesBegan(NSSet touches, UIEvent evt)
             {
                 base.TouchesBegan(touches, evt);
-                UITouch touch = touches.AnyObject as UITouch;
+                var touch = touches.AnyObject as UITouch;
                 
                 double positionX = -1;
                 double positionY = -1;
 
                 if (OnTap != null && touch != null)
                 {
-                    positionX = (double)touch.PreviousLocationInView(View).X;
-                    positionY = (double)touch.PreviousLocationInView(View).Y;
+                    positionX = touch.PreviousLocationInView(View).X;
+                    positionY = touch.PreviousLocationInView(View).Y;
                     OnTap(positionX, positionY);
                 }
 
@@ -192,15 +192,15 @@ namespace ScnViewGestures.Plugin.Forms.iOS.Renderers
             {
                 base.TouchesBegan(touches, evt);
 
-                UITouch touch = touches.AnyObject as UITouch;
+                var touch = touches.AnyObject as UITouch;
 
                 double positionX = -1;
                 double positionY = -1;
 
                 if (touch != null)
                 {
-                    positionX = (double)touch.PreviousLocationInView(View).X;
-                    positionY = (double)touch.PreviousLocationInView(View).Y;
+                    positionX = touch.PreviousLocationInView(View).X;
+                    positionY = touch.PreviousLocationInView(View).Y;
                 }
 
                 if (OnTouchesBegan != null)
@@ -245,15 +245,15 @@ namespace ScnViewGestures.Plugin.Forms.iOS.Renderers
             {
                 base.TouchesBegan(touches, evt);
 
-                UITouch touch = touches.AnyObject as UITouch;
+                var touch = touches.AnyObject as UITouch;
 
                 double positionX = -1;
                 double positionY = -1;
 
                 if (touch != null)
                 {
-                    positionX = (double)touch.PreviousLocationInView(View).X;
-                    positionY = (double)touch.PreviousLocationInView(View).Y;
+                    positionX = touch.PreviousLocationInView(View).X;
+                    positionY = touch.PreviousLocationInView(View).Y;
                 }
 
                 if (OnTouchesBegan != null)
@@ -286,15 +286,15 @@ namespace ScnViewGestures.Plugin.Forms.iOS.Renderers
             {
                 base.TouchesBegan(touches, evt);
 
-                UITouch touch = touches.AnyObject as UITouch;
+                var touch = touches.AnyObject as UITouch;
 
                 double positionX = -1;
                 double positionY = -1;
 
                 if (touch != null)
                 {
-                    positionX = (double)touch.PreviousLocationInView(View).X;
-                    positionY = (double)touch.PreviousLocationInView(View).Y;
+                    positionX = touch.PreviousLocationInView(View).X;
+                    positionY = touch.PreviousLocationInView(View).Y;
                 }
 
                 if (OnTouchesBegan != null)
@@ -312,12 +312,12 @@ namespace ScnViewGestures.Plugin.Forms.iOS.Renderers
             public override void TouchesMoved(NSSet touches, UIEvent evt)
             {
                 base.TouchesMoved(touches, evt);
-                UITouch touch = touches.AnyObject as UITouch;
+                var touch = touches.AnyObject as UITouch;
 
                 if (OnDrag != null && touch != null) 
                 {
-                    double offsetX = (double)touch.PreviousLocationInView(View).X - (double)touch.LocationInView(View).X;
-                    double offsetY = (double)touch.PreviousLocationInView(View).Y - (double)touch.LocationInView(View).Y;
+                    var offsetX = touch.PreviousLocationInView(View).X - (double)touch.LocationInView(View).X;
+                    var offsetY = touch.PreviousLocationInView(View).Y - (double)touch.LocationInView(View).Y;
                     OnDrag (-offsetX, -offsetY);
                 }
             }
